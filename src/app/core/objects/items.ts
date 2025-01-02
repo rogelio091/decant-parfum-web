29eb364fb720edc80450cbf274b093f9a03f20ea
Diego Granados <rogelio@users-MacBook-Pro.local>
2025-01-02 11:42:23 -0600

    rebase pathimage

--- src/app/core/objects/items.ts
+++ src/app/core/objects/items.ts
@@ -1,12 +1,14 @@
 import { Gender, Item, ItemStatus } from 'app/core/models/item.type';
 
+const image_basePath = 'images/fragances/';
+
 export const items_catalogue: Item[] = [
   {
     house: 'Adolfo Dominguez',
     name: 'Lima Tonka',
     description:
       'Combina notas vibrantes que evocan energía y vitalidad con una base cálida y suave, ideal para climas cálidos y uso diario. Su versatilidad la hace perfecta para hombres que buscan una esencia moderna y equilibrada.',
-    image: 'images/fragances/Limatonka.jpg',
+    image: image_basePath + 'Limatonka.jpg',
     notes: {
       top: 'lima, limón, acorde marino',
       middle: 'haba tonka, cedro, pachulí',
@@ -24,7 +26,7 @@ export const items_catalogue: Item[] = [
     name: 'Rehla',
     description:
       'Esta composición equilibrada la hace ideal para mujeres que buscan una fragancia dulce y sofisticada, adecuada para diversas ocasiones. Su longevidad y proyección son notables, asegurando una presencia duradera a lo largo del día.',
-    image: 'images/fragances/adyanrehla.jpg',
+    image: image_basePath + 'adyanrehla.jpg',
     notes: {
       top: 'pera, naranja tangerina, bergamota, lichi',
       middle: 'fresa, orquídea de vainilla, café',
@@ -42,7 +44,7 @@ export const items_catalogue: Item[] = [
     name: '9AM Dive',
     description:
       'Es valorada por su longevidad y proyección moderada, siendo adecuada para diversas ocasiones, especialmente en climas cálidos. Algunos usuarios han notado similitudes con fragancias de alta gama, considerándola una alternativa accesible sin comprometer la calidad.',
-    image: 'images/fragances/9amdive.webp',
+    image: image_basePath + '9amdive.webp',
     notes: {
       top: 'limón, menta, grosella negra, pimienta rosa',
       middle: 'manzana, incienso, cedro',
@@ -60,7 +62,7 @@ export const items_catalogue: Item[] = [
     name: '9AM Femme',
     description:
       'Esta composición equilibrada la hace ideal para mujeres que buscan una fragancia versátil y sofisticada, adecuada para diversas ocasiones. Su longevidad y proyección son notables, asegurando una presencia duradera a lo largo del día.',
-    image: 'images/fragances/9amfemme.webp',
+    image: image_basePath + '9amfemme.webp',
     notes: {
       top: 'mandarina, bergamota, toronja (pomelo)',
       middle: 'grosella negra, frambuesa',
@@ -78,7 +80,7 @@ export const items_catalogue: Item[] = [
     name: '9PM Femme',
     description:
       'Esta composición equilibrada la hace ideal para mujeres que buscan una fragancia versátil y sofisticada, adecuada para diversas ocasiones, especialmente para uso nocturno. Su longevidad y proyección son notables, asegurando una presencia duradera a lo largo de la noche.',
-    image: 'images/fragances/9pmfemme.webp',
+    image: image_basePath + '9pmfemme.webp',
     notes: {
       top: 'frambuesa, manzana, violeta, naranja',
       middle: 'rosa, peonía, iris, jazmín',
@@ -96,7 +98,7 @@ export const items_catalogue: Item[] = [
     name: '9PM Rebel',
     description:
       'Esta composición se abre con una explosión jugosa de frutas, seguida de un corazón amaderado y un fondo dulce y cálido, ofreciendo una experiencia olfativa equilibrada y versátil.',
-    image: 'images/fragances/9pmrebel.jpg',
+    image: image_basePath + '9pmrebel.jpg',
     notes: {
       top: 'mandarina, piña, manzana Granny Smith',
       middle: 'cedro, musgo de roble, vainilla',
@@ -115,7 +117,7 @@ export const items_catalogue: Item[] = [
     name: '9PM',
     description:
       'Es frecuentemente comparada con fragancias de diseñador como "Jean Paul Gaultier Ultra Male," siendo una alternativa más accesible pero igualmente sofisticada. Su proyección y longevidad son notables, lo que la convierte en una elección destacada para quienes buscan un perfume que deje huella.',
-    image: 'images/fragances/9pm.jpg',
+    image: image_basePath + '9pm.jpg',
     notes: {
       top: 'manzana, canela, lavanda silvestre, bergamota',
       middle: 'flor de azahar del naranjo, lirio de los valles (muguete)',
@@ -133,7 +135,7 @@ export const items_catalogue: Item[] = [
     name: 'Amber Oud Gold',
     description:
       'Se caracteriza por una apertura fresca con notas de bergamota y acordes verdes, que dan paso a un corazón dulce y afrutado de melón, piña y notas dulces, complementadas por ámbar. La base combina notas amaderadas, vainilla y almizcle, ofreciendo una estela cálida y envolvente.',
-    image: 'images/fragances/alharamainoudgold.webp',
+    image: image_basePath + 'alharamainoudgold.webp',
     notes: {
       top: 'bergamota, notas verdes',
       middle: 'melón, piña, notas dulces, ámbar',
@@ -152,7 +154,7 @@ export const items_catalogue: Item[] = [
     name: 'Nyla',
     description:
       'Esta composición se abre con una mezcla vibrante de frutas, coco y néctar de durazno, que aportan una dulzura tropical. El corazón floral, dominado por las flores de tiaré, añade una elegancia exótica. La base de sándalo y heliotropo proporciona una calidez cremosa y un toque empolvado, respectivamente, que redondean la fragancia con suavidad y profundidad.',
-    image: 'images/fragances/nyla.webp',
+    image: image_basePath + 'nyla.webp',
     notes: {
       top: 'acorde frutal, coco, néctar de durazno',
       middle: 'acorde floral, flores de tiaré',
@@ -170,7 +172,7 @@ export const items_catalogue: Item[] = [
     name: 'Odyssey Mandarin Sky',
     description:
       'Esta composición equilibrada la hace ideal para hombres que buscan una fragancia versátil y sofisticada, adecuada para diversas ocasiones. Su longevidad y proyección son notables, asegurando una presencia duradera a lo largo del día.',
-    image: 'images/fragances/mandarinsky.webp',
+    image: image_basePath + 'mandarinsky.webp',
     notes: {
       top: 'mandarina, naranja, azafrán, salvia',
       middle: 'caramelo, haba tonka, cempasúchil (tagete, clavelón)',
@@ -188,7 +190,7 @@ export const items_catalogue: Item[] = [
     name: 'Club de Nuit Intense EDP',
     description:
       'Esta composición se abre con una mezcla vibrante de cítricos y frutas, seguida de un corazón floral y amaderado, y culmina con una base cálida y sensual. Es ideal para hombres que buscan una fragancia versátil y duradera, adecuada para diversas ocasiones.',
-    image: 'images/fragances/clubdenuitedp.jpg',
+    image: image_basePath + 'clubdenuitedp.jpg',
     notes: {
       top: 'limón, piña, grosellas negras, bergamota, manzana',
       middle: 'abedul, jazmín, rosa',
@@ -207,7 +209,7 @@ export const items_catalogue: Item[] = [
     name: 'Club de Nuit Women EDP',
     description:
       'Esta composición se abre con notas cítricas y afrutadas, seguida de un corazón floral y exótico, y culmina con una base cálida y terrosa. Es ideal para mujeres que buscan una fragancia versátil y duradera, adecuada para diversas ocasiones.',
-    image: 'images/fragances/clubdenuitwomen.jpg',
+    image: image_basePath + 'clubdenuitwomen.jpg',
     notes: {
       top: 'naranja, toronja (pomelo), bergamota, durazno (melocotón)',
       middle: 'rosa, jazmín, geranio, lichi',
@@ -225,7 +227,7 @@ export const items_catalogue: Item[] = [
     name: 'The Icon Attitude',
     description:
       'Es una fragancia masculina de la familia olfativa aromática fougère, diseñada para hombres modernos y seguros de sí mismos. Esta Eau de Parfum se caracteriza por una composición equilibrada que combina notas frescas, dulces y amaderadas.',
-    image: 'images/fragances/banderasattitude.jpg',
+    image: image_basePath + 'banderasattitude.jpg',
     notes: {
       top: 'mandarina, néctar de pera, pimienta rosa',
       middle: 'lavanda, geranio, jazmín',
@@ -243,7 +245,7 @@ export const items_catalogue: Item[] = [
     name: 'King EDP',
     description:
       'Esta composición equilibrada la hace ideal para hombres que buscan una fragancia versátil y sofisticada, adecuada para diversas ocasiones. Su longevidad y proyección son notables, asegurando una presencia duradera a lo largo del día.',
-    image: 'images/fragances/bhararaking.webp',
+    image: image_basePath + 'bhararaking.webp',
     notes: {
       top: 'naranja, limón, bergamota',
       middle: 'notas afrutadas',
@@ -262,7 +264,7 @@ export const items_catalogue: Item[] = [
     name: 'King Gold Edition EDP',
     description:
       'Esta composición se abre con una explosión cítrica de naranja dulce, bergamota y limón, proporcionando una entrada fresca y energizante. El corazón introduce una mezcla dulce y exótica de frutos rojos y coco, aportando profundidad y carácter a la fragancia. Finalmente, la base combina almizcle blanco, vainilla y ámbar, ofreciendo una estela cálida y envolvente que perdura en la piel.',
-    image: 'images/fragances/bhararakinggold.webp',
+    image: image_basePath + 'bhararakinggold.webp',
     notes: {
       top: 'naranja dulce, bergamota, limón',
       middle: 'frutas rojas, coco',
@@ -280,7 +282,7 @@ export const items_catalogue: Item[] = [
     name: 'Rohit Azure',
     description:
       'Esta fragancia es ideal para el uso diario y ocasiones especiales, ofreciendo una experiencia olfativa que evoluciona a lo largo del día, desde una apertura vibrante hasta una base reconfortante.',
-    image: 'images/fragances/bharararohit.jpg',
+    image: image_basePath + 'bharararohit.jpg',
     notes: {
       top: 'bergamota, mandarina, naranja dulce, limón, jengibre',
       middle: 'cognac, mimosa francesa, neroli, osmanto, jazmín blanco',
@@ -298,7 +300,7 @@ export const items_catalogue: Item[] = [
     name: 'Rome Pour Femme',
     description:
       'Esta armoniosa fusión de notas la hace una fragancia cautivadora para la mujer moderna y segura de sí misma. Es ideal para diversas ocasiones, desde salidas casuales hasta eventos formales, y se adapta especialmente bien a las estaciones de primavera, otoño e invierno. Su duración es notable, permaneciendo en la piel entre 6 y 8 horas.',
-    image: 'images/fragances/bharararomefemme.webp',
+    image: image_basePath + 'bharararomefemme.webp',
     notes: {
       top: 'grosella negra, pimienta rosa, bergamota',
       middle: 'jazmín',
@@ -317,7 +319,7 @@ export const items_catalogue: Item[] = [
     name: 'Rome Pour Homme',
     description:
       'Esta composición se abre con la frescura de la lavanda, aportando un toque floral y calmante. En el corazón, la cálida vainilla añade una suavidad cremosa y dulce. La base está anclada por el carácter verde y amaderado del vetiver, proporcionando un acabado refinado y masculino.',
-    image: 'images/fragances/bharararomehomme.webp',
+    image: image_basePath + 'bharararomehomme.webp',
     notes: {
       top: 'lavanda',
       middle: 'vainilla',
@@ -336,7 +338,7 @@ export const items_catalogue: Item[] = [
     name: 'Velvet Rose',
     description:
       'Ha sido comparada con el perfume "Yara" de Lattafa, destacando por su desempeño superior en términos de duración y proyección. Es ideal para quienes buscan una fragancia dulce y floral con un toque oriental, adecuada para diversas ocasiones y estaciones del año.',
-    image: 'images/fragances/bhararavelvetrose.png',
+    image: image_basePath + 'bhararavelvetrose.png',
     notes: {
       top: 'orquídea, heliotropo, naranja tangerina',
       middle: 'acuerdo goloso, frutas tropicales',
@@ -355,7 +357,7 @@ export const items_catalogue: Item[] = [
     name: 'Women',
     description:
       'Apertura fresca y aromática, seguida de un corazón floral y amaderado, y culmina con una base cálida y reconfortante.',
-    image: 'images/fragances/burberrywomen.jpg',
+    image: image_basePath + 'burberrywomen.jpg',
     notes: {
       top: 'lavanda, bergamota, tomillo, menta',
       middle: 'geranio, sándalo, musgo, cedro',
@@ -374,7 +376,7 @@ export const items_catalogue: Item[] = [
     name: 'Hero EDP',
     description:
       'Apertura fresca y resinosa, seguida de un corazón cálido y especiado, y culmina con una base robusta y amaderada.',
-    image: 'images/fragances/burberryheroedp.jpg',
+    image: image_basePath + 'burberryheroedp.jpg',
     notes: {
       top: 'agujas de pino, incienso de olíbano (franquincienso)',
       middle: 'benjuí, incienso',
@@ -392,7 +394,7 @@ export const items_catalogue: Item[] = [
     name: '212 Men',
     description:
       'Apertura fresca y especiada, seguida de un corazón floral y aromático, y culmina con una base amaderada y almizclada que aporta profundidad y calidez a la fragancia.',
-    image: 'images/fragances/212men.png',
+    image: image_basePath + '212men.png',
     notes: {
       top: 'notas verdes, toronja (pomelo), especias, bergamota, lavanda, petit grain',
       middle: 'jengibre, violeta, gardenia, salvia',
@@ -411,7 +413,7 @@ export const items_catalogue: Item[] = [
     name: '212 VIP Black',
     description:
       'Apertura especiada y ligeramente amarga, seguida de un corazón aromático de lavanda, y culmina con una base dulce y almizclada que aporta profundidad y sensualidad a la fragancia.',
-    image: 'images/fragances/212vipblack.jpg',
+    image: image_basePath + '212vipblack.jpg',
     notes: {
       top: 'absenta, anís, hinojo',
       middle: 'lavanda',
@@ -430,7 +432,7 @@ export const items_catalogue: Item[] = [
     name: 'CH Men Pasión',
     description:
       'Apertura especiada y aromática, seguida de un corazón floral y amaderado, y culmina con una base cálida y profunda que aporta sofisticación y elegancia a la fragancia.',
-    image: 'images/fragances/chmenpasion.jpg',
+    image: image_basePath + 'chmenpasion.jpg',
     notes: {
       top: 'incienso de olíbano (franquincienso), neroli de Túnez, pimienta rosa',
       middle: 'iris, mirra, cedro de Texas',
@@ -448,7 +450,7 @@ export const items_catalogue: Item[] = [
     name: 'Happy For Men',
     description:
       'Apertura fresca y cítrica, seguida de un corazón floral suave, y culmina con una base amaderada y almizclada que aporta profundidad y calidez a la fragancia.',
-    image: 'images/fragances/cliniqueformen.webp',
+    image: image_basePath + 'cliniqueformen.webp',
     notes: {
       top: 'mandarina, lima, notas marinas, limón, notas verdes',
       middle: 'fresia, jazmín, lirio de los valles, rosa',
@@ -467,7 +469,7 @@ export const items_catalogue: Item[] = [
     name: 'Light Blue',
     description:
       'Apertura fresca y afrutada, seguida de un corazón floral delicado, y culmina con una base amaderada y almizclada que aporta profundidad y calidez a la fragancia.',
-    image: 'images/fragances/lightbluewomen.jpg',
+    image: image_basePath + 'lightbluewomen.jpg',
     notes: {
       top: 'limón siciliano, manzana, cedro, campanilla',
       middle: 'bambú, jazmín, rosa blanca',
