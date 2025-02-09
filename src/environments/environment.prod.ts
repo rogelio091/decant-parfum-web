a284733a521036d9f9ce05bb6d21ef112e9dfe78
Diego Granados <rogelio@users-MacBook-Pro.local>
2025-02-08 21:26:19 -0600

    fixi url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: true,
-  API_URL: 'https://perfumes-catalog.diegorogelio.workers.dev/',
+  API_URL: 'https://perfumes-catalog.diegorogelio.workers.dev',
   // ENPOINTS: Endpoints,
   S_KEY: 'HX0KSQPHrapmAZLPB'
 };
